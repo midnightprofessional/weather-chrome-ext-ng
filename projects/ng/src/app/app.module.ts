fbf867faa5a0705da8e0bb44319a9bbbb0366748
Ross Lin <ross.lin@gmail.com>
2021-08-24 10:35:21 +1000

    Scaffod Optiosn page

--- projects/ng/src/app/app.module.ts
+++ projects/ng/src/app/app.module.ts
@@ -3,19 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { createCustomElement } from '@angular/elements';
 
 import { PopupPageComponent } from './popup-page/popup-page.component';
+import { OptionsPageComponent } from './options-page/options-page.component';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [PopupPageComponent],
-  exports: [PopupPageComponent],
+  declarations: [PopupPageComponent, OptionsPageComponent],
+  exports: [PopupPageComponent, OptionsPageComponent],
 })
 export class AppModule implements DoBootstrap {
-  
+
   constructor(private injector: Injector) { }
 
   ngDoBootstrap(): void {
-    const customButton = createCustomElement(PopupPageComponent, { injector: this.injector });
-    customElements.define('popup-page', customButton);
+    const popup = createCustomElement(PopupPageComponent, { injector: this.injector });
+    customElements.define('popup-page', popup);
+
+    const options = createCustomElement(OptionsPageComponent, { injector: this.injector });
+    customElements.define('options-page', options);
   }
 
- }
+}
---
+++ projects/ng/src/app/options-page/options-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-options-page',
+  templateUrl: './options-page.component.html',
+  styleUrls: ['./options-page.component.scss'],
+  // encapsulation: ViewEncapsulation.ShadowDom
+})
+export class OptionsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
