d0badf5d0dc95e189094017cc2f4939ed7d2bbfc
Ross Lin <ross.lin@gmail.com>
2021-08-25 08:11:13 +1000

    Show home temp in badge

--- projects/chrome/background.ts
+++ projects/chrome/background.ts
@@ -1,5 +1,6 @@
 import weather from '../shared/open-weather.service';
 import storage from '../shared/local-storage.service';
+import { TempetureScale } from 'projects/shared/open-weather.data-model';
 
 console.info('Hi from background');
 
@@ -18,3 +19,18 @@ chrome.contextMenus.onClicked.addListener(async event => {
     await weather.fetchWeatherData(city);
     await storage.addCity(city);
 });
+
+let symbol = '';
+Promise.all([storage.getTempetureScale(), storage.getHomeCity()])
+    .then(([scale, home]) => {
+        symbol = scale === 'metric' ? '\u2103' : '\u2109';
+        return home ? weather.fetchWeatherData(home, scale) : undefined;
+    })
+    .then(weather => {
+        if (weather)
+            chrome.action.setBadgeText({
+                text: Math.round(weather.main.temp).toString() + symbol
+            });
+    })
+
+
