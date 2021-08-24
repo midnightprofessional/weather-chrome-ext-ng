94d4d8ec7c1541bd2e9a82319d3eac9a7296f0a9
Ross Lin <ross.lin@gmail.com>
2021-08-24 11:22:25 +1000

    Implemented city input and scale toggle functions

--- projects/ng/src/app/popup-page/popup-page.component.ts
+++ projects/ng/src/app/popup-page/popup-page.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
+import { OpenWeatherData, TempetureScale } from 'projects/shared/open-weather.data-model';
+import weather from 'projects/shared/open-weather.service';
+import storage from 'projects/shared/local-storage.service';
+
 @Component({
   selector: 'app-popup-page',
   templateUrl: './popup-page.component.html',
@@ -8,9 +12,76 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class PopupPageComponent implements OnInit {
 
+  loading = false;
+  errMsg?: string;
+
+  scale?: TempetureScale;
+
+  cities: string[] = [];
+  weatherDatas: OpenWeatherData[] = [];
+
+
   constructor() { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    try {
+      this.loading = true;
+      this.scale = await storage.getTempetureScale();
+      // this.homeCity = await storage.getHomeCity();
+      // if (this.homeCity)
+      //   this.homeCityWeatherData = await weather.fetchWeatherData(this.homeCity, this.scale)
+      this.cities = await storage.getCities();
+      this.weatherDatas = await this.getWeatherData(this.cities, this.scale);
+    } finally {
+      this.loading = false;
+    }
+
+  }
+
+  onKeyUp() {
+    if (this.errMsg)
+      delete this.errMsg;
+  }
+
+  async onAddCity(input: HTMLInputElement) {
+    try {
+      this.loading = true;
+
+      const city = input.value;
+      if (!city)
+        throw new Error('city is empty');
+
+      if (this.cities.some(v => v.toUpperCase() === city.toUpperCase()))
+        throw new Error(`${city} is already added`);
+
+      this.weatherDatas = await this.getWeatherData([...this.cities, city], this.scale);
+      this.cities = await storage.addCity(city);
+
+      input.value = '';
+    } catch (err) {
+      this.errMsg = err.message;
+    } finally {
+      this.loading = false;
+    }
+  }
+
+  async onScaleToggle() {
+    try {
+      this.loading = true;
+
+      this.scale = this.scale === 'metric' ? 'imperial' : 'metric';
+      await storage.setTempetureScale(this.scale);
+      // if (this.homeCity)
+      //   this.homeCityWeatherData = await weather.fetchWeatherData(this.homeCity, this.scale)
+      this.weatherDatas = await this.getWeatherData(this.cities, this.scale);
+    } finally {
+      this.loading = false;
+    }
+
+  }
+
+  private async getWeatherData(cities: string[], scale: TempetureScale = 'metric'): Promise<OpenWeatherData[]> {
+    return await Promise.all(cities.map(c => weather.fetchWeatherData(c, scale)));
   }
 
 }
